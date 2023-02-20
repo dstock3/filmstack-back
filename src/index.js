19deddba45ed8f21e2de0453fd447b5af77160b2
David Stockdale <dstock3@gmail.com>
2023-02-20 01:07:22 -0600

    added models folder and initialized context for test data.

--- src/index.js
+++ src/index.js
@@ -2,60 +2,45 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
+import models from './models';
 
 const app = express();
+
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get('/', (req, res) => {
+  console.log(models)
   res.send('root get request');
 });
 
-let users = {
-  1: {
-    id: '1',
-    username: 'Johnny Hopkins',
-  },
-  2: {
-    id: '2',
-    username: 'Sloan Kettering',
-  }
-};
-
-let directMessages = {
-  1: {
-    id: 1,
-    text: 'Hello World',
-  },
-  2: {
-    id: 2,
-    text: 'Hello World 2',
-  },
-  3: {
-    id: 3,
-    text: 'Hello World 3',
-  },
-  4: {
-    id: 4,
-    text: 'Hello World 4',
-  }
-};
+app.get('/session/', (req, res) => {
+  return res.send(req.context.models.users[req.context.me.id]);
+});
 
 app.get('/users', (req, res) => {
-  return res.send(Object.values(users));
+  return res.send(Object.values(req.context.models.users));
 });
 
 app.get('/users/:userid', (req, res) => {
-  return res.send(users[req.params.userid]);
+  return res.send(req.context.models.users[req.params.userid]);
 });
 
 app.get('/direct-messages', (req, res) => {
-  return res.send(Object.values(directMessages));
+  return res.send(Object.values(req.context.models.directMessages));
 });
 
 app.get('/direct-messages/:dmid', (req, res) => {
-  return res.send(directMessages[req.params.dmid]);
+  return res.send(req.context.models.directMessages[req.params.dmid]);
 });
 
 app.post('/direct-messages', (req, res) => {
@@ -63,8 +48,9 @@ app.post('/direct-messages', (req, res) => {
   const directMessage = {
     id,
     text: req.body.text,
+    userId: req.context.me.id,
   };
-  directMessages[id] = directMessage;
+  req.context.models.directMessages[id] = directMessage;
   return res.send(directMessage);
 });
 
@@ -72,14 +58,10 @@ app.delete('/direct-messages/:dmid', (req, res) => {
   const { 
     [req.params.dmid]: directMessage,
     ...otherMessages 
-  } = directMessages;
-  
-  directMessages = otherMessages;
-  return res.send(directMessage);
-});
+  } = req.context.models.directMessages;
 
-app.get('/session/', (req, res) => {
-  return res.send(req.me.id);
+  req.context.models.directMessages = otherMessages;
+  return res.send(directMessage);
 });
 
 app.listen(process.env.PORT, () =>
---
+++ src/models/index.js
@@ -0,0 +1,34 @@
+let users = {
+    1: {
+        id: '1',
+        username: 'Johnny Hopkins',
+    },
+    2: {
+        id: '2',
+        username: 'Sloan Kettering',
+    }
+};
+  
+let directMessages = {
+    1: {
+        id: 1,
+        text: 'Hello World',
+    },
+    2: {
+        id: 2,
+        text: 'Hello World 2',
+    },
+    3: {
+        id: 3,
+        text: 'Hello World 3',
+    },
+    4: {
+        id: 4,
+        text: 'Hello World 4',
+    }
+};
+
+export default {
+    users,
+    directMessages,
+};
