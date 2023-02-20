5920ae12e8b1122821d741f17b232cbd52ccb17e
David Stockdale <dstock3@gmail.com>
2023-02-20 00:26:40 -0600

    Added middleware.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Received a GET HTTP method!');
+  res.send('root get request');
 });
 
-app.post('/', (req, res) => {
-  res.send('Received a POST HTTP method!');
+app.post('direct-messages', (req, res) => {
+  /*
+  const id = uuidv4();
+  const directMessage = {
+    id,
+    text: req.body.text,
+  };
+  directMessages[id] = directMessage;
+  return res.send(directMessage);
+  */
 });
 
-app.put('/', (req, res) => {
-  res.send('Received a PUT HTTP method!');
-});
-
-app.delete('/', (req, res) => {
-  res.send('Received a DELETE HTTP method!');
+app.get('direct-messages/:dmid', (req, res) => {
+  return res.sendStatus(directMessages[req.params.dmid]);
+  
 });
 
 app.listen(process.env.PORT, () =>
