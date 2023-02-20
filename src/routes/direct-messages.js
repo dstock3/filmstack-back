6ee79548ff51d70f2121ee865510dc426b5647d9
David Stockdale <dstock3@gmail.com>
2023-02-20 01:19:00 -0600

    added routes for direct messages.

---
+++ src/routes/direct-messages.js
@@ -0,0 +1,38 @@
+import { v4 as uuidv4 } from 'uuid';
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  return res.send(Object.values(req.context.models.directMessages));
+});
+
+router.get('/:messageId', (req, res) => {
+  return res.send(req.context.models.directMessages[req.params.dmid]);
+});
+
+router.post('/', (req, res) => {
+  const id = uuidv4();
+  const directMessage = {
+    id,
+    text: req.body.text,
+    userId: req.context.me.id,
+  };
+
+  req.context.models.directMessages[id] = directMessage;
+
+  return res.send(directMessage);
+});
+
+router.delete('/:messageId', (req, res) => {
+  const {
+    [req.params.dmid]: message,
+    ...otherMessages
+  } = req.context.models.directMessages;
+
+  req.context.models.directMessages = otherMessages;
+
+  return res.send(directMessage);
+});
+
+export default router;
