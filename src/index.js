50ec321a558c051ba3d4de0c6f53c94b35624352
David Stockdale <dstock3@gmail.com>
2023-02-21 00:26:49 -0600

    removed unnecessary import and installed new dependencies.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
 import models from './models';
 import routes from './routes';
 
